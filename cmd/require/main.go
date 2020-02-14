3ef6c4a39fac2d315ec8d701aedfe395767f8c4a
marguerite <i@marguerite.su>
2020-02-14 14:35:12 +0800

    require generator

--- cmd/require/main.go
+++ cmd/require/main.go
@@ -2,35 +2,80 @@ package main
 
 import (
 	"fmt"
+	simplejson "github.com/bitly/go-simplejson"
+	semver "github.com/openSUSE-zh/node-semver"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
-	//"github.com/marguerite/util/dir"
-	//simplejson "github.com/bitly/go-simplejson"
 )
 
+func parse(f string) *simplejson.Json {
+	f1, _ := ioutil.ReadFile(f)
+	json, _ := simplejson.NewJson(f1)
+	return json
+}
+
 func main() {
-  for _, v := range os.Args[1:] {
-	  fmt.Println(v)
-  }
-  args := os.Args[1:]
-  //args, _ := dir.Ls("/home/marguerite/home:MargueriteSu:branches:devel:languages:nodejs/gulp/usr/lib/node_modules")
-  var files []string
-  // old style rpm dependency generator, all files are passed through stdin, filter the package.json
-  if len(args) > 1 {
-	  for _, v := range args {
-		  // both package.json in node_modules and bower_components
-		  if strings.HasSuffix(v, "package.json") {
-			  files = append(files, v)
-		  }
-	  }
-  }
+	args := os.Args[1:]
+	//f, _ := ioutil.ReadFile("1.txt")
+	//args := strings.Split(string(f), "\n")
+	var files []string
+	// old style rpm dependency generator, all files are passed through stdin, filter the package.json
+	if len(args) > 1 {
+		for _, v := range args {
+			// both package.json in node_modules and bower_components
+			if strings.HasSuffix(v, "package.json") {
+				files = append(files, v)
+			}
+		}
+	} else {
+		files = args
+	}
+	provided := make(map[string]string)
+	required := make(map[string]semver.Range)
 
-  for _, v := range files {
-    if strings.Contains(v, "bower_components") {
+	for _, v := range files {
+		if strings.Contains(v, "bower_components") {
 
-    } else {
-      fmt.Println(v)
-    }
-  }
+		} else if strings.Contains(filepath.Base(filepath.Dir(v)), "@") {
+			json := parse(v)
+			// provided
+			a := strings.Split(filepath.Base(filepath.Dir(v)), "@")
+			if _, ok := provided[a[0]]; !ok {
+				provided[a[0]] = a[1]
+			}
+			// required
+			for k, v := range json.Get("dependencies").MustMap() {
+				if _, ok := required[k]; !ok {
+					v1, _ := v.(string)
+					required[k] = semver.NewRange(v1)
+				}
+			}
+		}
+	}
 
+	isolated := make(map[string][]string)
+	for k, v := range required {
+		if _, ok := provided[k]; ok {
+			if v.Satisfy(semver.NewSemver(provided[k])) {
+				continue
+			} else {
+				//fmt.Println(k)
+				//fmt.Println(provided[k])
+				//fmt.Println(v.String())
+			}
+		}
+		if _, ok := isolated[k]; !ok {
+			isolated[k] = strings.Split(v.String(), " ")
+		}
+	}
+
+	if ! (isolated == nil) {
+		for k, v := range isolated {
+                   for _, v1 := range v {
+			fmt.Println("node_module("+k+") "+v1)
+		   }
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ require (
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/marguerite/util v0.0.0-20191130060341-008b03ab3d6b
+	github.com/openSUSE-zh/node-semver v0.0.0-20200209091312-556591635086
 )
